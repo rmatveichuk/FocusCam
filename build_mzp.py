e44412b7f6c50eb6c4d14f120e4b3bd1cbb477d9
rmatveichuk <r.matveichuk@gmail.com>
2026-07-27 17:52:27 +0300

    Update FocusCam_UI.mcr with sys.modules purge to force reload Python code on macro launch

--- build_mzp.py
+++ build_mzp.py
@@ -26,7 +26,7 @@ tooltip:"FocusCam"
 iconName:"Icon"
 (
     on execute do (
-        python.Execute "import sys; usr_scr = __import__('pymxs').runtime.getDir(__import__('pymxs').runtime.Name('userScripts')); sys.path.append(usr_scr) if usr_scr not in sys.path else None; import FocusCam.focus_manager; FocusCam.focus_manager.show_focus_window()"
+        python.Execute "import sys, gc; usr_scr = __import__('pymxs').runtime.getDir(__import__('pymxs').runtime.Name('userScripts')); sys.path.append(usr_scr) if usr_scr not in sys.path else None; to_del = [m for m in list(sys.modules.keys()) if 'focus' in m.lower() or 'batch' in m.lower()]; [sys.modules.pop(m, None) for m in to_del]; gc.collect(); import FocusCam.focus_manager; FocusCam.focus_manager.show_focus_window()"
     )
 )
 """
